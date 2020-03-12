e1f669cb7ca22f71da50b7cc9376f06baffde1ae
ray <ryan.chiu@hotmail.com>
2020-03-12 08:06:49 +0000

    show all the office in "WEEKLY SALES PER OFFICE" insted of only top 10s.

--- app/vendors/top10s.php
+++ app/vendors/top10s.php
@@ -29,23 +29,23 @@ $_sql_ =
 		and stats.agentid = accounts.id and agents.id = stats.agentid 
 		and agents.companyid = companies.id 
 		AND agentid > 0 
-	GROUP BY %s  ORDER BY `sales` desc  LIMIT 10;";
-$sql = sprintf($_sql_, 0, $today, "convert(trxtime, date) >= '2016-08-14' and convert(trxtime, date) <= '$today'", 'agentid');
+	GROUP BY %s  ORDER BY `sales` desc  LIMIT %d;";
+$sql = sprintf($_sql_, 0, $today, "convert(trxtime, date) >= '2016-08-14' and convert(trxtime, date) <= '$today'", 'agentid', 10);
 $rs = mysql_query("delete from top10s where flag = 0", $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
 $rs = mysql_query("insert into top10s " . $sql, $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
-$sql = sprintf($_sql_, 1, $today, "convert(trxtime, date) <= '$weekend' AND convert(trxtime, date) >= '$weekstart'", 'agentid');
+$sql = sprintf($_sql_, 1, $today, "convert(trxtime, date) <= '$weekend' AND convert(trxtime, date) >= '$weekstart'", 'agentid', 10);
 $rs = mysql_query("delete from top10s where flag = 1", $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
 $rs = mysql_query("insert into top10s " . $sql, $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
-$sql = sprintf($_sql_, 2, $today, "convert(trxtime, date) <= '$monthend' AND convert(trxtime, date) >= '$monthstart'", 'agentid');
+$sql = sprintf($_sql_, 2, $today, "convert(trxtime, date) <= '$monthend' AND convert(trxtime, date) >= '$monthstart'", 'agentid', 10);
 $rs = mysql_query("delete from top10s where flag = 2", $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
 $rs = mysql_query("insert into top10s " . $sql, $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
-$sql = sprintf($_sql_, 3, $today, "convert(trxtime, date) <= '$weekend' AND convert(trxtime, date) >= '$weekstart'", 'officename');
+$sql = sprintf($_sql_, 3, $today, "convert(trxtime, date) <= '$weekend' AND convert(trxtime, date) >= '$weekstart'", 'officename', 1000);
 $rs = mysql_query("delete from top10s where flag = 3", $zconn->dblink)
 	or die ("Something wrong with: " . mysql_error() . "\n");
 $rs = mysql_query("insert into top10s " . $sql, $zconn->dblink)
