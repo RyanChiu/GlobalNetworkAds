aee297e965a3472a48f876445819f59ae83c86b3
ray <ryan.chiu@hotmail.com>
2020-08-20 12:16:46 +0000

    show up/down arrow when sort

--- app/View/Helper/ExPaginatorHelper.php
+++ app/View/Helper/ExPaginatorHelper.php
@@ -24,6 +24,17 @@ class ExPaginatorHelper extends PaginatorHelper {
 		}
 		
 		$options['direction'] = 'desc';
+		
+		$options['escape'] = false;
+		$options['class'] .= " text-reset";
+		if (strpos($options['class'], "asc") !== false 
+			|| strpos($options['class'], "desc") !== false) {
+			if ($sortDir == 'desc') {
+				$title .= '<i class="icon-arrow-down" style="font-size:12px;margin:0 0 0 3px;"></i>'; //show down arrow
+			} else {
+				$title .= '<i class="icon-arrow-up" style="font-size:12px;margin:0 0 0 3px;"></i>'; //show up arrow
+			}
+		}
  
 		return parent::sort($key, $title, $options);
  
